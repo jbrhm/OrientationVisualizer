f8ea09d0d6497dc96790662985b2f7340202ecbd
jbrhm <jrabraham1904@gmail.com>
2024-04-17 17:54:15 -0400

    idk instance or smthn

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
+// Define the flag that we are using webgpu_hpp
+#define WEBGPU_CPP_IMPLEMENTATION
+
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include <webgpu/webgpu.h>
+
 
 int main (int, char**){
 	WGPUInstanceDescriptor desc = {};
 	desc.nextInChain = nullptr;
 
+	//Create te WebGPU instance
 	WGPUInstance instance = wgpuCreateInstance(&desc);
 	std::cout << "Instance: " << instance << std::endl;
+
+	// Get the wgpu adapter
+	//WGPURequestAdapterOptions adapterOpts = {};
+	//WGPUAdapter adapter = requestAdapter(instance, &adapterOpts);
+
+	// Destroy the WebGPU instance
+	wgpuInstanceRelease(instance);
 	return 0;
 }
