53221af269d4b71175d81ad8e1fa0ec33f0156da
jbrhm <jrabraham1904@gmail.com>
2024-06-30 22:34:11 -0400

    Viz now using better compile definitions

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -1,3 +1,7 @@
 macro(set_source_directory var dir)
-    file(GLOB_RECURSE ${var} CONFIGURE_DEPENDS ${dir}/*)
+    file(GLOB_RECURSE ${var} CONFIGURE_DEPENDS ${dir}/*c*)
+endmacro()
+
+macro(set_include_directory var dir)
+	file(GLOB_RECURSE ${var} CONFIGURE_DEPENDS ${dir}*.h*)
 endmacro()
