5c665be3b4537b9b49b73f053408ffd931f3cbf2
jbrhm <jrabraham1904@gmail.com>
2024-07-02 09:31:45 -0400

    Gathering deps 2

--- cmake/binaries.cmake
+++ cmake/binaries.cmake
@@ -1,2 +1,15 @@
 install(FILES ${CMAKE_BINARY_DIR}/libwgpu_native.so DESTINATION lib)
-install(FILES /lib/x86_64-linux-gnu/libstdc++.so.6 DESTINATION lib)
+install(CODE [[
+        file(GET_RUNTIME_DEPENDENCIES
+            EXECUTABLES ./Viz
+            RESOLVED_DEPENDENCIES_VAR _r_deps
+        )
+        foreach(_file ${_r_deps})
+            file(INSTALL
+                DESTINATION "${CMAKE_INSTALL_PREFIX}/lib"
+                TYPE SHARED_LIBRARY
+                FOLLOW_SYMLINK_CHAIN
+                FILES "${_file}"
+            )
+        endforeach()
+      ]])
