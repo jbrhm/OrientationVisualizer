1b6a2a4447b8cf8748b37718444121c451ca6867
jbrhm <jrabraham1904@gmail.com>
2024-09-19 15:48:26 -0400

    Working on bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,6 +8,8 @@ git clone https://github.com/jbrhm/OrientationVisualizer.git
 
 cd ~/OrientationVisualizer
 
+./ansible/ansible.sh ./ansible/dev.yml
+
 cmake . -B build
 cmake --build build
 sudo cmake --install build
